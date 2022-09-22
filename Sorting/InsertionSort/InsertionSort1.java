ad52bfaf041341e659ede66d2e39c69b71a56315
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-09-22 12:14:16 +0530

    Insertion Sort completed

---
+++ Sorting/InsertionSort/InsertionSort1.java
@@ -0,0 +1,31 @@
+class InsertionSort1 {
+    public static void main(String[] args) {
+        int[] myArray = {20,35,14,-15,55,77,1};
+
+        System.out.print("Original Array is : ");
+        print(myArray);
+
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex < myArray.length;firstUnsortedIndex++){
+            int newElement = myArray[firstUnsortedIndex];
+
+            int i;
+
+            for(i= firstUnsortedIndex;i>0 && myArray[i -1]> newElement;i--){
+                myArray[i] = myArray[i-1];
+            }
+
+            myArray[i] = newElement;
+        }
+
+
+
+        System.out.print("\nSorted Array using Selectin Sort : ");
+        print(myArray);
+    }
+
+    public static void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
